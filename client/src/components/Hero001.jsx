c15416a4c368017ea5ca600cee07edd98250b27a
stephen100mwangi <mwangiwahome70@gmail.com>
2024-03-28 20:29:06 +0300

    Third commit

--- client/src/components/Hero001.jsx
+++ client/src/components/Hero001.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Hero001 = () => {
+    const images = ["/assets/lights.jpg","/assets/kitchen.jpg","/assets/bed__.jpg","/assets/bed_.jpg","/assets/bed.jpg"]
+    const [random,setRandom] = useState(0);
+    const [randomText,setRandomText] = useState(0);
+
+    const titles = ["Beyond Listings Beyond Limits: Your Perfect AirBNB Awaits","Your Airbnb soulmate: We'll match you with your dream stay.","Airbnb smarter: Find the perfect fit, faster."]
+
+   
+    useEffect(()=>{
+        setInterval(() => {
+            const randomNumber = Math.floor(Math.random()* 5)
+            setRandom( randomNumber)
+        }, 10000);
+
+        setInterval(() => {
+            const random_Number = Math.floor(Math.random()*3);
+            setRandomText(random_Number)
+
+        }, 12000);
+    },[])
 
     return (
     <div className='bg-background h-[calc(100vh-120px)] px-[32px] flex flex-col relative z-[1000]'>
@@ -13,17 +33,17 @@ const Hero001 = () => {
             <p className='text-2xl text-hero mt-20 font-medium z-[1000] relative'>Adventure awaits.Find your next unforgettable Airbnb.</p>
         </div>
         <div>
-            <p className='text-3xl w-2/5 text-heroTitle leading-[50px] mt-10 font-bold'>Beyond Listings <br /> Beyond Limits: <br /> Your Perfect <br /> AirBNB Awaits</p>
+            <p className='text-3xl w-1/5 text-heroTitle leading-[50px] mt-10 font-bold relative'>{titles[randomText]}</p>
         </div>
 
         <div>
-            <p className='mt-6 w-2/5 text-justify font-light text-heroTitle'>
+            <p className='mt-6 w-2/5 text-justify font-light text-heroTitle relative'>
                 Stop searching.Start Living.Discover hidden gems.charming studios and unforgettable adventures.Find an airbnb that feels like home no matter where you are.
             </p>
         </div>
 
-        <div className='rounded-tl-none rounded-bl-full rounded-br-none rounded-tr-full w-1/2 h-20 absolute right-[50px] z-[100] top-8'>
-            <img src="/assets/bed__.jpg" alt=""  className='rounded-tl-none rounded-bl-full rounded-br-none rounded-tr-full object-cover z-[0] rounded-2xl'/>
+        <div className='rounded-tl-none rounded-bl-full rounded-br-none rounded-tr-full w-1/2 h-[450px] absolute right-[50px] z-[100] top-0'>
+            <img src={images[random]} alt=""  className='rounded-tl-none rounded-bl-full rounded-br-none rounded-tr-full object-cover z-[0] rounded-2xl h-[100%] w-[100%]'/>
         </div>
 
     </div>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About Us
+    <div className='bg-background h-[calc(100vh-120px)] relative'>
+      <div className="bg-hero size-[350px] rounded-br-full absolute left-10 rounded-tr-full rounded-bl-3xl rounded-tl-full ">
+        <img src="/assets/bed__.jpg" className='object-cover rounded-br-full w-[100%] h-[100%] rounded-tr-full rounded-bl-3xl rounded-tl-full hue-rotate-90' alt="" />
+      </div>
+      <div className='w-1/2 absolute right-5 text-right'>
+        <p className='text-heroTitle leading-8 tracking-wider'>
+          Welcome to <span className='text-hero'>BetterRooms</span>, your personalized gateway to unforgettable travel experiences!At Airbnb Recommender, we believe that every journey should be as unique as you are. That's why we've created a cutting-edge recommender system designed to match you with the perfect accommodations for your next adventure. Whether you're seeking a cozy cabin in the woods, a stylish urban loft, or a charming beachfront villa, we've got you covered.
+        </p>
+      </div>
+      <div className='w-[700px] h-[250px] px-5 absolute bottom-5 border-none rounded-lg shadow-2xl right-5 flex space-x-10 overflow-x-auto scrollbar-hide'>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Customer Support Integration</p>
+        </div>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Continuous Improvement</p>
+        </div>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Dynamic Pricing Recommendations</p>
+        </div>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Host Listing Optimization</p>
+        </div>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Enhanced Search Experience</p>
+        </div>
+        <div className='h-[240px] w-[200px] min-w-[200px] shadow-inner text-black my-auto rounded-lg flex items-center text-center justify-center'>
+          <p>Personalized Accommodation Recommendations</p>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
